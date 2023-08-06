df52bef0f5da73a56a8322d6d2a7876d4531a359
Jose <joserujume@gmail.com>
2023-08-06 05:23:30 -0600

    REMOVE: Remove tinylicious

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core'
 import { SharedMap, SharedString } from 'fluid-framework'
 import { AzureClient } from '@fluidframework/azure-client'
 import { InsecureTokenProvider } from '@fluidframework/test-client-utils'
-// import { TinyliciousClient } from '@fluidframework/tinylicious-client'
 
 @Component({
   selector: 'app-root',
@@ -45,14 +44,5 @@ export class AppComponent implements OnInit {
     // console.log(id)
     this.fluidContainer = await this.client.getContainer('2164a0e1-fba8-4300-9bc0-b6cc4b143e3a', this.schema)
     this.sharedDescription = await this.fluidContainer.container.initialObjects.description
-
-    // this.client = new TinyliciousClient()
-    // // this.fluidContainer = await this.client.createContainer(this.schema)
-    // // const id = await this.fluidContainer.container.attach()
-    // // console.log(id)
-    // // this.fluidContainer = await this.client.getContainer('132db1ab-8f80-4ce6-8fde-1b7121c9c0e3', this.schema)
-    // this.fluidContainer = await this.client.getContainer('be9ca19f-3bda-4342-a674-1d42e98226ed', this.schema)
-
-    // this.sharedDescription = await this.fluidContainer.container.initialObjects.description
   }
 }
