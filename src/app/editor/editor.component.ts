d41acb35ed1f62e0e8a7ff82f7312558ac51b44d
Jose <joserujume@gmail.com>
2023-08-16 15:48:14 -0600

    UPDATE: Replace method

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -43,28 +43,23 @@ export class CollaborativeTextAreaComponent implements OnInit, AfterViewInit {
 
     const client = new TinyliciousClient()
     // this.fluidContainer = await client.createContainer(this.schema); const id = await this.fluidContainer.container.attach(); console.log(id)
-    this.fluidContainer = await client.getContainer('86d85f79-b1bd-43a8-b5ba-ad7bef7fcd41', this.schema)
+    this.fluidContainer = await client.getContainer('760b7a07-0ce8-406a-8f9a-87484d4c62f6', this.schema)
     this.sharedDescription = this.fluidContainer.container.initialObjects.description as SharedString
 
     this.description = this.sharedDescription.getText()
 
     this.syncData()
 
-    // ToDo: initialize editor here
+    // Editor initialization
     this.editor = this.editorEl.quill
     this.editor.container.style.whiteSpace = 'pre-line'
 
   }
 
   syncData(): void {
-    // Sets an event listener so we can update our state as the value changes
     this.sharedDescription.on('sequenceDelta', (event: SequenceDeltaEvent) => {
       console.log('sequenceDelta')
       console.log(event)
-      // We only need to insert if the text changed.
-      // if (this.sharedDescription.getText() === this.description) {
-      //   return
-      // }
 
       // If the event is our own then just insert the text
       if (event.isLocal) {
@@ -72,29 +67,41 @@ export class CollaborativeTextAreaComponent implements OnInit, AfterViewInit {
         return
       }
 
-      // Because we did not make the change we need to manage the remote
-      // character insertion.
-      const remoteCaretStart = event.first.position
-      const remoteCaretEnd = event.last.position + event.last.segment.cachedLength
-
-      if (remoteCaretStart < this.selectionStart){
-        this.selectionStart = this.selectionStart + event.last.segment.cachedLength
-      }
-
-      setTimeout(() => this.editor.setSelection(this.selectionStart, 0), 0)
+      // Because we did not make the change we need to manage the remote character insertion.
+      this.setCaretPosition(event.first, event.last, event.opArgs.op, this.sharedDescription.getText() )
 
       this.description = this.sharedDescription.getText()
       // this.description = ''
     })
   }
 
-  setCaretPosition(newStart: number, newEnd: number): void {
-    // setTimeout(() => this.editor.setSelection(newStart, 0), 0)
+  setCaretPosition(first: any, last: any, op: any, sharedDescription: any): void {
+
+    // if (description.length === this.description.length)
+
+    if (first.position < this.selectionStart){
+      console.log('selectionStart ' + this.selectionStart)
+      console.log('description.length ' + this.description.length)
+      console.log('sharedDescription.length ' + sharedDescription.length)
+      console.log(this.selectionStart - Math.abs(this.description.length - sharedDescription.length))
+
+      if (this.description.length > sharedDescription.length){
+        console.log('small')
+        this.selectionStart = this.selectionStart - Math.abs(this.description.length - sharedDescription.length)
+      } else {
+        console.log('bigger')
+        this.selectionStart = this.selectionStart + Math.abs(this.description.length - sharedDescription.length)
+      }
+
+    }
+
+    console.log('selectionStart ' + this.selectionStart)
+    setTimeout(() => this.editor.setSelection(this.selectionStart, 0), 0)
   }
 
   onSelectionChange(event: any): void {
     console.log('onSelectionChange')
-    // console.log(event)
+    console.log(event)
 
     if (event.source === 'api'){
       console.log(event)
@@ -102,7 +109,6 @@ export class CollaborativeTextAreaComponent implements OnInit, AfterViewInit {
 
     if (event.source === 'user'){
       if (this.editor.getSelection()){
-        console.log(this.editor.getSelection())
         this.selectionStart = event.range.index
         this.selectionEnd = event.range.index + event.range.length
         console.log('selectionStart ' + this.selectionStart)
@@ -119,60 +125,67 @@ export class CollaborativeTextAreaComponent implements OnInit, AfterViewInit {
   onTextChange(event: any): void {
     console.log('onTextChange')
     console.log(event.delta)
-    if (this.quillGetDeltaDelete(event.delta) && this.quillGetDeltaInsert(event.delta)){
+    if (this.getDeltaDelete(event.delta) && this.getDeltaInsert(event.delta)){
       console.log('replace')
-      this.sharedDescription.replaceText(this.selectionStart, this.selectionEnd, this.quillGetDeltaInsert(event.delta))
-    } else if (this.quillGetDeltaDelete(event.delta) && !this.quillGetDeltaInsert(event.delta)){
+      this.sharedDescription.replaceText(this.getDeltaPosition(event.delta), +this.getDeltaPosition(event.delta) + +this.getDeltaDelete(event.delta), this.getDeltaInsert(event.delta))
+    } else if (this.getDeltaDelete(event.delta) && !this.getDeltaInsert(event.delta)){
       console.log('remove')
-      this.sharedDescription.removeText(this.editor.getSelection().index, +this.editor.getSelection().index + +this.quillGetDeltaDelete(event.delta))
+      this.sharedDescription.removeText(this.editor.getSelection().index, +this.editor.getSelection().index + +this.getDeltaDelete(event.delta))
     } else {
       console.log('insert')
-      console.log(event.delta.ops)
+      // console.log(event.delta.ops)
       // console.log('selectionStart ' + this.selectionStart)
       // console.log('editor.getSelection().index ' + this.editor.getSelection().index)
       // console.log('quillGetDeltaPosition(event.delta) ' + this.quillGetDeltaPosition(event.delta))
       // if (event.delta.ops.length > 1){
-        this.sharedDescription.insertText(this.quillGetDeltaPosition(event.delta), this.quillGetDeltaInsert(event.delta))
+      //   console.log(true)
+      // }
+        console.log('quillGetDeltaPosition(event.delta) ' + this.getDeltaPosition(event.delta))
+        this.sharedDescription.insertText(this.getDeltaPosition(event.delta), this.getDeltaInsert(event.delta))
       // } else {
         // this.sharedDescription.insertText(this.quillGetDeltaPosition(event.delta), this.quillGetDeltaInsert(event.delta))
       // }
     }
 
-    if (this.quillGetDeltaAttributes(event.delta)){
-      console.log(JSON.parse(this.quillGetDeltaAttributes(event.delta)))
+    if (this.getDeltaAttributes(event.delta)){
       console.log('attributes')
-      console.log('pos1 ' + event.delta.ops[0].retain)
-      console.log('pos2 ' + (+event.delta.ops[0].retain + +event.delta.ops[1].retain))
-      this.sharedDescription.annotateRange(event.delta.ops[0].retain, +event.delta.ops[0].retain + +event.delta.ops[1].retain, JSON.parse(this.quillGetDeltaAttributes(event.delta)))
+      // console.log(JSON.parse(this.getDeltaAttributes(event.delta)))
+      // console.log('pos1 ' + event.delta.ops[0].retain)
+      // console.log('pos2 ' + (+event.delta.ops[0].retain + +event.delta.ops[1].retain))
+      // this.sharedDescription.annotateRange(this.getDeltaRange(event.delta)[0], +this.getDeltaRange(event.delta)[0] + +this.getDeltaRange(event.delta)[1], JSON.parse(this.getDeltaAttributes(event.delta)))
     }
 
-    this.selectionStart = this.quillGetDeltaPosition(event.delta)
-    this.selectionEnd = +this.quillGetDeltaPosition(event.delta) + +this.quillGetDeltaInsert(event.delta).length
-    console.log('selectionStart ' + this.selectionStart)
-    console.log('selectionEnd ' + this.selectionEnd)
+    this.selectionStart = this.getDeltaPosition(event.delta)
+    this.selectionEnd = +this.getDeltaPosition(event.delta) + +this.getDeltaInsert(event.delta).length
+    // console.log('selectionStart ' + this.selectionStart)
+    // console.log('selectionEnd ' + this.selectionEnd)
     console.log('')
     console.log('')
   }
 
-  onInitEditor(event: any): void{
-    // console.log(event.editor)
-    // this.editor = event.editor
-    // this.editor.container.style.whiteSpace = 'pre-line'
+  getDeltaPosition(delta: any): number {
+    let retain = 0
+    delta.map((op: any) => {
+      if (typeof op.retain === 'number') {
+        retain = op.retain
+      }
+    })
+    return retain
   }
 
-  quillGetDeltaPosition(delta: any): number {
-    // const retain = delta.map((op: any) => {
-    //   if (typeof op.retain === 'number') {
-    //     return op.retain
-    //   } else {
-    //     return ''
-    //   }
-    // }).join('')
-    const retain = delta.ops[0].retain
-    return retain !== '' && retain !== undefined ? retain : 0
+  getDeltaRange(delta: any): number[] {
+    const retain: any[] = []
+    delta.map((op: any) => {
+      if (op.retain === '' || op.retain === undefined ){
+        retain.push(0)
+      } else {
+        retain.push(op.retain)
+      }
+    })
+    return retain
   }
 
-  quillGetDeltaInsert(delta: any): string {
+  getDeltaInsert(delta: any): string {
     return delta.map((op: any) => {
       if (typeof op.insert === 'string') {
         return op.insert
@@ -182,7 +195,7 @@ export class CollaborativeTextAreaComponent implements OnInit, AfterViewInit {
     }).join('')
   }
 
-  quillGetDeltaDelete(delta: any): number {
+  getDeltaDelete(delta: any): number {
     const del = delta.map((op: any) => {
       if (typeof op.delete === 'number') {
         return op.delete
@@ -193,7 +206,7 @@ export class CollaborativeTextAreaComponent implements OnInit, AfterViewInit {
     return del !== '' ? del : 0
   }
 
-  quillGetDeltaAttributes(delta: any): any {
+  getDeltaAttributes(delta: any): any {
     return delta.map((op: any) => {
       if (op.attributes) {
         return JSON.stringify(op.attributes)
